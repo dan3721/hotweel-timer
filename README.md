a778cc0e830f4fed54e46af1312c1483829c0d4f
Dan Bush <dan.bush@gmail.com>
2019-09-25 16:45:11 -0400

    README and save latest code

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Hot Wheels Timer
+
+[photos](https://photos.app.goo.gl/9hNKVoD95w1RYyw16)
+
+# Hardware
+
+Two [IR Infrared Obstacle Avoidance Sensor Modules](https://www.amazon.com/gp/product/B01I57HIJ0/ref=ppx_yo_dt_b_asin_image_o08_s00?ie=UTF8&psc=1) 
+are suspended over the track and  placed some distance apart.
+
+# Software
+
+The software is a JavaScript program using the [onoff](https://www.npmjs.com/package/onoff) and library. For more 
+details see index.js
+
--- index.js
+++ index.js
@@ -1,33 +1,90 @@
+/**
+* Hotwheels speed trap. The setup consists of a Raspberry Pi and two infrared gates. As the 
+* car passes the first gate the timer is started. Upon passing through the second gate the 
+* elapsed run time is calculated and reported. 
+* 
+* Note: You can activate the averages mode by setting NUM_TRIAL_RUNS greater than one. In 
+* this mode, the average speed will also be reported on the final trial run.
+*
+* @version 2.0.0
+*
+* @author Braden Bush
+* @author Dan Bush
+*/
+
 const Gpio = require('onoff').Gpio
 
-const DEBOUNCE_TIMEOUT = 250
-const GATE_DISTANCE_IN_INCHES = 5
-const GATE_DISTANCE_IN_MILES = GATE_DISTANCE_IN_INCHES/63360  
+const NUM_TRIAL_RUNS = 3 		// if > 1 will average runs
+const GATE_DISTANCE_IN_INCHES = 50	// distance between the gates in inches
+                                        
+const GATE_DISTANCE_IN_MILES = GATE_DISTANCE_IN_INCHES/63360                                                                                                       
+const DEBOUNCE_TIMEOUT = 500
 
-//const gate1a = new Gpio(23, 'in', 'falling')
-const gate1a = new Gpio(23, 'in', 'falling', {debounceTimeout: DEBOUNCE_TIMEOUT})
-const gate1b = new Gpio(24, 'in', 'falling', {debounceTimeout: DEBOUNCE_TIMEOUT})
+// Configure the start gate to GPIO 23 and the end gate to GPIO 24.
+//
+// IR sensors are configured by dialing the onboard potentiometer by starting with the 
+// signal LOW and then dialing until the SIGNAL goes HIGH. Orignally we configured the 
+// reverse but, ultimatly we got better results with the various cars this way.
 
-let start
+const GATE_START = new Gpio(23, 'in', 'both', {debounceTimeout: DEBOUNCE_TIMEOUT})
+const GATE_FINISH = new Gpio(24, 'in', 'both', {debounceTimeout: DEBOUNCE_TIMEOUT})
 
-gate1a.watch((err, value) => {
-//  console.log(value, err)
-  start = new Date()
-//  console.log('timing...')
+// Setup global timing flags and variables.
+let timing = false
+let numRuns = 0
+let startTime
+let ACCUMULATIVE_RUN_TIME = 0
+
+// Calculates speed in miles per hour given the elapsed time and the known distance between
+// the gates.
+function calculateSpeedInMPH(elapsedTime) {
+  const AVERAGE_TIME_IN_HOURS = elapsedTime/1000/60/60
+  const SPEED_IN_MPH = +(Math.round(GATE_DISTANCE_IN_MILES/AVERAGE_TIME_IN_HOURS +'e+3') + 'e-3')
+  return SPEED_IN_MPH
+}
+
+// Resets the associated timing flags and global variables.
+function reset() {
+  timing = false
+  numRuns = 0
+  startTime = 0
+  ACCUMULATIVE_RUN_TIME = 0
+}
+
+// Start gate handeling.
+GATE_START.watch((err, value) => {
+  timing = true
+  startTime = new Date()
 });
 
-gate1b.watch((err, value) => {
-  const ELAPSED_TIME_IN_HOURS = (new Date()-start)/1000/60/60
-  const SPEED_IN_MPH = GATE_DISTANCE_IN_MILES/ELAPSED_TIME_IN_HOURS
-  const SPEED_IN_MPH_ROUNDED = +(Math.round(SPEED_IN_MPH +'e+3') + 'e-3')
-  console.log('mph: ', SPEED_IN_MPH_ROUNDED) 
+// Finish gate handeling.
+GATE_FINISH.watch((err, value) => {
+  if (!timing) {
+    console.warn('Finish gate tripped before the start gate!')
+  }
+  else {
+    numRuns++
+    const ELAPSED_RUN_TIME = new Date()-startTime
+    if (NUM_TRIAL_RUNS > 1) {
+      ACCUMULATIVE_RUN_TIME += ELAPSED_RUN_TIME
+      console.log(calculateSpeedInMPH(ELAPSED_RUN_TIME) + ' mph (' +numRuns+' of '+NUM_TRIAL_RUNS+')')
+      if (numRuns === NUM_TRIAL_RUNS) {
+        const AVERAGE_ELAPSED_TIME = ACCUMULATIVE_RUN_TIME/NUM_TRIAL_RUNS
+        console.log('average mph: '+ calculateSpeedInMPH(AVERAGE_ELAPSED_TIME))
+        reset()
+      }
+    }
+    else {
+      console.log(calculateSpeedInMPH(ELAPSED_RUN_TIME)+' mph')
+      reset()
+    }
+  }
 });
 
-
-console.log('started')
-
+// Releases resources on exit.
 process.on('SIGINT', () => {
-  gate1a.unexport()
-  gate1b.unexport()
+  GATE_START.unexport()
+  GATE_FINISH.unexport()
 });
 
+console.log('Start you engines!')
